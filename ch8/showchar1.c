323c6834270bcd9efe61dad4553614592b68415b
picongzhi <picongzhi@gmail.com>
2017-11-11 21:23:11 +0800

    add some examples

---
+++ ch8/showchar1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+void display(char ch, int lines, int width);
+
+int main(void) {
+    int ch;
+    int rows, cols;
+    printf("Enter a character and two integers:\n");
+    while ((ch = getchar()) != '\n') {
+        scanf("%d %d", &rows, &cols);
+        display(ch, rows, cols);
+        printf("Enter another character and two integers;\n");
+        printf("Enter a newline to quit.\n");
+    }
+    printf("Bye.\n");
+}
+
+void display(char ch, int lines, int width) {
+    int row, col;
+
+    for (row = 0; row < lines; row++) {
+        for (col = 0; col < width; col++)
+            putchar(ch);
+        putchar('\n');
+    }
+}
